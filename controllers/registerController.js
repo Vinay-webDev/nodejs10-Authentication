69a5d13276f6c4b33d72de42ec755b2793852caa
Vinay-webDev <justchatting3000@gmail.com>
2024-09-21 23:56:31 +0530

    2. handleNewUsers complete✅

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -9,6 +9,7 @@ const usersDB = {
 // now we need some common core modules
 const fsPromises = require('fs').promises;
 const path = require('path');
+const bcrypt = require('bcrypt');
 // now we need to install a dependency called bcrypt which will do hash , salt passwords
 //===>> npm i bcrypt
 /////////////
@@ -19,14 +20,33 @@ const handleNewUsers = async(req, res) => {
     const { user, pwd } = req.body;
     //[2] now we need to check for if the user and pwd exist
     if (!user || !pwd) {
-        res.sendStatus(400).json({"message":"username and password are required!"});
+        res.status(400).json({"message":"username and password are required!"});
     }
     //if we are in this step means we already have a user and pwd
-    
+    //[3] we need to check for the duplicate username in DB (database);
+    const duplicate = usersDB.users.find( u => u.username === user);
+    //[4] if there is duplicate user then response would be (409) 
+    if (duplicate) return res.sendStatus(409) // 409 means conflict***
+    //[5] try and catch block
+    try {
+        //[7] encrypt the pwd ==>> now we need to hash and salt with bcrypt 
+        const hashedPwd = await bcrypt.hash(pwd, 10) // 10 salt round which is default;
+        //[8] we need to store newUsers in DB
+        const newUser = {"username":user, "password":hashedPwd};
+        usersDB.setUsers([...usersDB.users, newUser]); // also we need to have the old users and new users combined***
+        //[9] need to write to a file as well ***
+        await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json' ), JSON.stringify(usersDB.users));
+        //aslo we need to log it to the console to cross check
+        console.log(usersDB.users);
+        res.status(201).json({"success":`new user${user} created!`});
+    } catch(err) {
+        //[6] let's handle the error part 
+        res.status(500).json({"message":err.message}); // 500 means server error!
+    }
 }
 
 
-
+module.exports = { handleNewUsers };
 
 
 
